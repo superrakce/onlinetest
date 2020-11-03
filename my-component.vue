cab9aca512487d87738abed80af9405331abc0ab
wilson.wu <wilson.wu@rs7-11.com>
2020-11-03 17:32:02 +0800

    1

---
+++ my-component.vue
@@ -0,0 +1,22 @@
+﻿<template>
+    <div>
+        <p>{{ greeting }} World!</p>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        data: function () {
+            return {
+                greeting: 'Hello'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    p {
+        font-size: 2em;
+        text-align: center;
+    }
+</style>
